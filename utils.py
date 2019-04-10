4451506ad04d28b94717bb4425db1243f9657c51
gustaf jacobzon <gustaf.jacobzon@gmail.com>
2019-04-10 14:15:20 +0200

    update

--- utils.py
+++ utils.py
@@ -180,41 +180,6 @@ def log_gaussian(x, mean, stddev=1.0):
 def mse(diff):
     return tf.sqrt(2 * tf.nn.l2_loss(diff))
 
-
-def pull_away_term(mb_latent, mb_size):
-    norm_z = mb_latent / tf.sqrt(tf.reduce_sum(tf.square(mb_latent), axis=1, keepdims=True))
-    affinity = tf.matmul(norm_z, norm_z, transpose_b=True)
-    pt_loss = (tf.reduce_sum(affinity) - mb_size) / ((mb_size - 1) * mb_size)
-
-    return pt_loss
-
-
-def kl_from_hist(p, q):
-    q[q == 0] = 1e-8
-    q = q/np.sum(q)
-    return np.sum(np.where(p != 0., p * np.log(p / q), 0.0))
-
-
-# https://github.com/rothk/Stabilizing_GANs # added batch_size arg, removed assert since mb_size is dynamic
-def discriminator_regularizer(D1_logits, D1_arg, D2_logits, D2_arg, batch_size):
-    D1 = tf.nn.sigmoid(D1_logits)
-    D2 = tf.nn.sigmoid(D2_logits)
-    grad_D1_logits = tf.gradients(D1_logits, D1_arg)[0]
-    grad_D2_logits = tf.gradients(D2_logits, D2_arg)[0]
-    # grad_D1_logits_norm = tf.norm(tf.reshape(grad_D1_logits, [batch_size,-1]), axis=1, keepdims=True)
-    # grad_D2_logits_norm = tf.norm(tf.reshape(grad_D2_logits, [batch_size,-1]), axis=1, keepdims=True)
-    grad_D1_logits_norm = tf.norm(grad_D1_logits, axis=1, keepdims=True)
-    grad_D2_logits_norm = tf.norm(grad_D2_logits, axis=1, keepdims=True)
-
-
-    #set keep_dims=True/False such that grad_D_logits_norm.shape == D.shape
-    # assert grad_D1_logits_norm.shape == D1.shape
-    # assert grad_D2_logits_norm.shape == D2.shape
-
-    reg_D1 = tf.multiply(tf.square(1.0-D1), tf.square(grad_D1_logits_norm))
-    reg_D2 = tf.multiply(tf.square(D2), tf.square(grad_D2_logits_norm))
-    disc_regularizer = tf.reduce_mean(reg_D1 + reg_D2)
-    return disc_regularizer
 if __name__=='__main__':
     # create_feature_csv()
     write2file('./imgs/')
